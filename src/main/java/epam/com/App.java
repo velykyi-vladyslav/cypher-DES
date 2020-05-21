a2c8e537d56264184869956917e43d96e42c7597
vladyslavvelykiy <vladyslavvelikiy777@gmail.com>
2020-05-21 23:02:34 +0300

    end of algorythm

--- src/main/java/epam/com/App.java
+++ src/main/java/epam/com/App.java
@@ -6,13 +6,21 @@ import epam.com.model.step2.InitialPermutation;
 import epam.com.model.step2.SBoxe;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         Encrypt encrypt = new Encrypt(16);
-        encrypt.encrypt();
+        encrypt.finalEncrypt();
         System.out.println(encrypt.toString());
+
+
     }
+
 
 }
--- src/main/java/epam/com/model/step2/Encrypt.java
+++ src/main/java/epam/com/model/step2/Encrypt.java
@@ -2,12 +2,13 @@ package epam.com.model.step2;
 
 import epam.com.model.step1.BitsOperations;
 import epam.com.model.step1.Key;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.awt.*;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 
 public class Encrypt {
@@ -35,6 +36,7 @@ public class Encrypt {
             35, 3, 43, 11, 51, 19, 59, 27,
             34, 2, 42, 10, 50, 18, 58, 26,
             33, 1, 41, 9, 49, 17, 57, 25};
+    private static Logger logger = LogManager.getLogger(Encrypt.class);
     private int rounds;
     private InitialPermutation ip;
     private Key key;
@@ -45,6 +47,9 @@ public class Encrypt {
     private SBoxe sBoxe;
     private BitsOperations bitsOperations;
     private List<String> encrypted;
+    private List<String> rightParts;
+    private List<String> leftParts;
+
 
 
     public Encrypt(int rounds) throws UnsupportedEncodingException {
@@ -57,29 +62,38 @@ public class Encrypt {
         r = ip.getIPRigthParts();
         keyN = key.getKeyN();
         encrypted = new ArrayList<>();
+
     }
 
-    public void encrypt() {
-        String twoPartsAfterFunc = "";
+    public void finalEncrypt() {
+        for (int i = 0; i < r.size(); i++) {
+            encrypt(r.get(i), l.get(i));
+        }
+    }
+
+    public void encrypt(String rightPart, String leftPart) {
+        String twoPartsAfterFunc;
         bitsOperations = new BitsOperations();
         List<String> afterPermitation = new ArrayList<>();
-
+        leftParts = createList(leftPart);
+        rightParts = createList(rightPart);
         for (int i = 1; i <= rounds; i++) {
-            l.set(i, r.get(i - 1));
-            r.set(i, calcFunc(i));
-            twoPartsAfterFunc = r.get(i)+l.get(i);
-            afterPermitation.add(bitsOperations.doPermutation(IP_TRANSPOSITED, twoPartsAfterFunc));
+            leftParts.set(i, rightParts.get(i - 1));
+            rightParts.set(i, calcFunc(i));
         }
-        for (String str: encrypted) {
-           encrypted.add(String.valueOf(Integer.parseInt(str, 16)));
+        twoPartsAfterFunc = leftParts.get(rounds) + rightParts.get(rounds);
+        afterPermitation.add(bitsOperations.doPermutation(IP_TRANSPOSITED, twoPartsAfterFunc));
+
+        for (String str : afterPermitation) {
+            encrypted.add(convertFromBaseToBase(str, 2, 16));
         }
     }
 
     private String doXorKnERn(int index) {
-        String Rn = expand(index);
-        String Kn = keyN.get(index);
-        xor = new Xor(Kn, Rn);
-        return xor.doXor();
+        String expandedR = expandByE(index - 1);//expanded by table E
+        String Kn = keyN.get(index - 1); //got Kn
+        xor = new Xor(Kn, expandedR);
+        return xor.doXor();//returns XORed result: Kn + E(Rn-1)
     }
 
     private List<String> splitIntoB(String KnRn) {
@@ -98,33 +112,54 @@ public class Encrypt {
         List<String> SbBlocks;
         String function = "";
         SbBlocks = calcSBox(splitIntoB(doXorKnERn(index)));
-        bitsOperations =new BitsOperations();
+        bitsOperations = new BitsOperations();
         for (String str : SbBlocks) {
             function = function + str;
         }
-        return bitsOperations.doPermutation(P,function );
+        logger.debug(function + " func");
+        return bitsOperations.doPermutation(P, function);
     }
 
-    private String expand(int index) {
+    private String expandByE(int index) {
         BitsOperations bo = new BitsOperations();
-        return bo.doPermutation(E, r.get(index - 1));
+        return bo.doPermutation(E, rightParts.get(index));
     }
 
     private List<String> calcSBox(List<String> b) {
         List<String> sFromB = new ArrayList<>();
+        logger.debug("B - boxes: " + b);
         int i = 0;
         for (String bN : b) {
             sBoxe = new SBoxe(bN, i);
             sFromB.add(sBoxe.calculate());
             i++;
         }
+        logger.debug("S(b): " + sFromB);
         return sFromB;
+    }
+
+    private List<String> createList(String part) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <= rounds; i++) {
+            if (i == 0) {
+                list.add(part);
+            } else
+                list.add("");
+        }
+        return list;
+    }
+
+    private String convertFromBaseToBase(String str, int base1, int base2) {
+        logger.info("str - " + str);
+        BigInteger bi = new BigInteger(str, base1);
+        logger.info(bi);
+        return bi.toString(base2);
     }
 
     @Override
     public String toString() {
         return "Encrypt{" +
-                "encrypted=" + encrypted +
+                "encrypted=" + encrypted.toString() +
                 '}';
     }
 }
